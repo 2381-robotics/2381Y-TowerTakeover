d7e4aeae3b1aa4db2c565c446f52a098ac7cc320
Qwerty Dvorak <51385923+Danflora@users.noreply.github.com>
2019-06-30 11:16:29 -0400

    Add files via upload

---
+++ everything.h
@@ -0,0 +1,17 @@
+#ifndef EVERYTHING_H_
+#define EVERYTHING_H_
+
+#define LF_PORT 20
+#define LB_PORT 10
+#define RF_PORT 11
+#define RB_PORT 1
+#define L1_PORT 2
+#define L2_PORT 6
+#define TEST_PORT 7
+#define VISION_PORT 5
+#define EXAMPLE_SIG 1
+
+#define LIFT_POWER 100
+
+
+#endif
