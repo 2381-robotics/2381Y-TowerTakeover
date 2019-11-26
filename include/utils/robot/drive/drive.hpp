9103fbba391cec87249d7a8d260944ca7c69be53
Jeffrey Li <dr.cheddar@outlook.com>
2019-11-25 22:28:16 -0500

    fixed type info

--- include/utils/robot/drive/drive.hpp
+++ include/utils/robot/drive/drive.hpp
@@ -9,11 +9,11 @@ public:
   virtual void Set_Point_Drive(double speed, double direction, double distance) = 0;
   virtual double Get_Speed() = 0;
   virtual double Get_Distance() = 0;
-  virtual void Reset_Point();
-  virtual void Set_Init_Point();
-  virtual bool get_running();
-  virtual void Set_Turn(double Speed, double direction, double distance);
-  // virtual ~Drive();
+  virtual void Reset_Point() = 0;
+  virtual void Set_Init_Point()= 0;
+  virtual bool get_running() = 0;
+  virtual void Set_Turn(double Speed, double direction, double distance) = 0;
+  // ~Drive();
   // virtual void Pid_Set_Point_Drive();
 
   // private:
